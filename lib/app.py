b989ed4b606321f93a19bb5d19e2ca1e8a05e8e5
Bett <bettpatrick080@gmail.com>
2024-02-29 22:08:54 +0300

    Completed Assignment

---
+++ lib/app.py
@@ -0,0 +1,11 @@
+# lib/app.py
+# print("Hello world!", end=" ")
+# print("Hello sun!", end="!! ")
+# print("Hello sky!", end="!!!\n")
+
+def test_app_py_exists():
+    assert(path.exists("lib/app.py"))
+
+print("Hello World!", end=" ")
+print("Pass this test, please.")
+
